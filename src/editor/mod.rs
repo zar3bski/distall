812a2f389334fcda37dbdae45a888e6d79b0ba95
zar3bski <zarebskidavid@gmail.com>
2025-07-17 17:19:27 +0200

    theme loaded

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -1,7 +1,8 @@
 use atomic_float::AtomicF32;
 use nih_plug::prelude::{util, Editor};
 use nih_plug_vizia::vizia::prelude::*;
-use nih_plug_vizia::widgets::*;
+
+use nih_plug_vizia::widgets::{ParamSlider, ParamSliderExt, PeakMeter, ResizeHandle};
 use nih_plug_vizia::{assets, create_vizia_editor, ViziaState, ViziaTheming};
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
@@ -19,7 +20,7 @@ impl Model for Data {}
 
 // Makes sense to also define this here, makes it a bit easier to keep track of
 pub(crate) fn default_state() -> Arc<ViziaState> {
-    ViziaState::new(|| (200, 150))
+    ViziaState::new(|| (400, 150))
 }
 
 pub(crate) fn create(
@@ -37,33 +38,36 @@ pub(crate) fn create(
         }
         .build(cx);
 
-        cx.add_stylesheet(include_style!("src/editor/style.css"))
+        cx.add_stylesheet(include_style!("src/editor/theme.css"))
             .expect("Failed to load stylesheet");
 
-        VStack::new(cx, |cx| {
-            Label::new(cx, "Pre-Gain");
-            //.font_family(vec![FamilyOwned::Name(String::from(assets::NOTO_SANS))])
-            //.font_weight(FontWeightKeyword::Thin)
-            //.height(Pixels(50.0))
-            //.child_top(Stretch(1.0))
-            //.child_bottom(Pixels(0.0));
+        HStack::new(cx, |cx| {
+            VStack::new(cx, |cx| {
+                Label::new(cx, "Pre-Gain");
 
-            Label::new(cx, "Gain");
-            ParamSlider::new(cx, Data::params, |params| &params.pre_gain);
+                ParamSlider::new(cx, Data::params, |params| &params.pre_gain);
 
-            PeakMeter::new(
-                cx,
-                Data::peak_meter
-                    .map(|peak_meter| util::gain_to_db(peak_meter.load(Ordering::Relaxed))),
-                Some(Duration::from_millis(600)),
-            )
-            // This is how adding padding works in vizia
-            .top(Pixels(10.0));
-        })
-        .row_between(Pixels(0.0))
-        .child_left(Stretch(1.0))
-        .child_right(Stretch(1.0));
+                PeakMeter::new(
+                    cx,
+                    Data::peak_meter
+                        .map(|peak_meter| util::gain_to_db(peak_meter.load(Ordering::Relaxed))),
+                    Some(Duration::from_millis(600)),
+                )
+                // This is how adding padding works in vizia
+                .top(Pixels(10.0));
+            })
+            .row_between(Pixels(0.0))
+            .child_left(Stretch(1.0))
+            .child_right(Stretch(1.0));
+            VStack::new(cx, |cx| {
+                Label::new(cx, "Post-Gain");
 
+                ParamSlider::new(cx, Data::params, |params| &params.post_gain);
+            })
+            .row_between(Pixels(0.0))
+            .child_left(Stretch(1.0))
+            .child_right(Stretch(1.0));
+        });
         ResizeHandle::new(cx);
     })
 }
