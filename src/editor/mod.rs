170551738143f5002e9ce9a98ef8495941ec11a8
zar3bski <zarebskidavid@gmail.com>
2025-07-22 13:02:18 +0200

    feat: btn switch between distortions

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -1,13 +1,16 @@
 use atomic_float::AtomicF32;
-use nih_plug::prelude::{util, Editor};
+use nih_plug::params::{Param, Params};
+use nih_plug::prelude::{util, Editor, GuiContext, ParamSetter};
+use nih_plug_vizia::vizia::context::EventContext;
 use nih_plug_vizia::vizia::prelude::*;
 
-use nih_plug_vizia::widgets::{ParamSlider, ParamSliderExt, PeakMeter, ResizeHandle};
+use nih_plug_vizia::widgets::{ParamEvent, ParamSlider, ParamSliderExt, PeakMeter, ResizeHandle};
 use nih_plug_vizia::{assets, create_vizia_editor, ViziaState, ViziaTheming};
 use std::sync::atomic::Ordering;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
+use crate::distortions::DistortionType;
 use crate::DistAllParams;
 
 #[derive(Lens)]
@@ -16,7 +19,33 @@ struct Data {
     peak_meter: Arc<AtomicF32>,
 }
 
-impl Model for Data {}
+pub enum AppEvent {
+    Increment,
+    Decrement,
+}
+
+impl Model for Data {
+    fn event(&mut self, cx: &mut EventContext, event: &mut Event) {
+        let param = &self.params.distortion;
+        event.map(|app_event, meta| match app_event {
+            AppEvent::Decrement => {
+                cx.emit(ParamEvent::BeginSetParameter(param).upcast());
+                cx.emit(
+                    ParamEvent::SetParameter(param, param.previous_step(param.value(), false))
+                        .upcast(),
+                );
+                cx.emit(ParamEvent::EndSetParameter(param).upcast());
+            }
+            AppEvent::Increment => {
+                cx.emit(ParamEvent::BeginSetParameter(param).upcast());
+                cx.emit(
+                    ParamEvent::SetParameter(param, param.next_step(param.value(), false)).upcast(),
+                );
+                cx.emit(ParamEvent::EndSetParameter(param).upcast());
+            }
+        });
+    }
+}
 
 // Makes sense to also define this here, makes it a bit easier to keep track of
 pub(crate) fn default_state() -> Arc<ViziaState> {
@@ -59,6 +88,24 @@ pub(crate) fn create(
             .row_between(Pixels(0.0))
             .child_left(Stretch(1.0))
             .child_right(Stretch(1.0));
+
+            VStack::new(cx, |cx: &mut Context| {
+                Label::new(cx, "Distortion");
+                HStack::new(cx, |cx: &mut Context| {
+                    Button::new(
+                        cx,
+                        |ex| ex.emit(AppEvent::Decrement),
+                        |cx| Label::new(cx, "<<"),
+                    );
+                    Label::new(cx, &Data::params.get(cx).distortion.to_string()); //FIXME: MAJ
+                                                                                  //Label::new(cx, &Data::params.get_val(cx).distortion.to_string());
+                    Button::new(
+                        cx,
+                        |ex| ex.emit(AppEvent::Increment),
+                        |cx| Label::new(cx, ">>"),
+                    );
+                });
+            });
             VStack::new(cx, |cx| {
                 Label::new(cx, "Post-Gain");
 
